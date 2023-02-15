bd599884d9f624b3d57d8a63f1853a86a34962ea
sinangho <shindirn@naver.com>
2023-02-15 19:05:32 +0900

    23 02 14

---
+++ 230215/7568.py
@@ -0,0 +1,21 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+info = []
+rank = []
+for i in range(N):
+    temp = list(map(int, sys.stdin.readline().rstrip().split()))
+    info.append(temp)
+    
+for i in info:
+    w = i[0]
+    h = i[1]
+    cnt = 1
+    for j in info:
+        if w < j[0] and h < j[1]:
+            cnt += 1
+    rank.append(cnt)
+
+for i in rank:
+    print(i, end=" ")
--- test.py
+++ test.py
@@ -1,13 +1,11 @@
-import sys
-from collections import deque
+N = int(input())
+lst = list()
+for i in range(N):
+    lst.append(input().split())
 
-N,M,V = map(int, sys.stdin.readline().split())
-a_list = [[False]*(N+1)for i in range(N+1)] 
-visited = [False]*(N+1)
-
-
-for i in range(M):
-    a, b =  map(int,sys.stdin.readline().split())
-    a_list[a][b] = a_list[b][a] = True
-
-print(a_list)
+for i in range(N):
+    grade = 1
+    for j in range(N):
+        if lst[i][0] < lst[j][0] and lst[i][1] < lst[j][1]:
+            grade = grade + 1
+    print(grade,end=" ")
