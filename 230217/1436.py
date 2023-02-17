696d820230caa16c9bca726bf2bac3244d6295c3
sinangho <shindirn@naver.com>
2023-02-18 00:20:33 +0900

    23 02 18

---
+++ 230217/1436.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+cnt = 0
+movie = 666
+while True:
+    if "666" in str(movie):
+        cnt += 1
+        
+    if cnt == N:
+        break
+    
+    movie += 1
+    
+print(movie)
+    
