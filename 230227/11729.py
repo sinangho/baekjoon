903e85dc175afbf2256b4d48ed4fa0d7dbf2cea5
sinangho <shindirn@naver.com>
2023-02-27 22:02:33 +0900

    23 02 26

---
+++ 230227/11729.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+def hanoi(N, From, To):
+    if N > 1:
+        hanoi(N-1, From, 6-From-To)
+    
+    print(From, To)
+    
+    if N > 1:
+        hanoi(N-1, 6-From-To, To)
+        
+print(2**N - 1)
+hanoi(N,1,3)
+        
