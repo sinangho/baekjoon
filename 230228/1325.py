cfe3b5ff440f19730b4d52c7458d68666cfc300c
sinangho <shindirn@naver.com>
2023-02-28 23:15:34 +0900

    23 02 28

---
+++ 230228/1325.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+
+visited = [0]*(N+1)
+graph = [[] for _ in range(N+1)]
+
+def BFS(v):
+    queue = deque([v])
+    visited = [False] * (N+1)
+    visited[v] = True
+    cnt = 1
+    
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+                cnt+=1
+    
+    return cnt
+
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    graph[b].append(a)
+    
+answer = []
+max = -1
+
+for i in range(1, N+1):
+    result = BFS(i)
+    if result > max:
+        answer = [i]
+        max = answer
+    elif answer == max:
+        answer.append(i)
+        
+for i in answer:
+    print(i, end=" ")5 4
+3 1
+3 2
+4 3
+5 3
---
+++ 230228/1427.py
@@ -0,0 +1,6 @@
+N = list(map(int, input()))
+
+N.sort(reverse=True)
+
+for i in N:
+    print(i,end="")
